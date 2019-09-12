4bf1033401657068b7e0570923acb376aa24df30
Mohammed Almukhtar <msalmukhtar@sbgtv.com>
2019-09-12 12:53:13 -0700

    adding some interactivity

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RecipeList from './RecipeList';
-import '../css/app.css'
+import '../css/app.css';
+import uuidv4 from 'uuid/v4';
 
 function App() {
-  
+  const [recipes, setRecipes] = useState(sampleRecipes)
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      cookTime: '1:00',
+      servings: 1,
+      instructions: 'Instr.',
+      ingredients: [
+        { id: uuidv4(), name: 'Name', amount: '1 Tbs' }
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
   return (
-    <RecipeList recipes={sampleRecipes} />
+    <RecipeList 
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete = {handleRecipeDelete}  
+    />
   );
   
 }
 
+
 const sampleRecipes = [
   {
-    id: 1,
+    id: uuidv4(),
     name: 'Plain Chicken',
     cookTime: '1:45',
     servings: 3,
     instructions: '1- Put Salt on Chicken \n2- Put chicken in oven \n3- Eat chicken',
     ingredients: [
       {
-        id: 1,
+        id: uuidv4(),
         name: 'Chicken',
         amount: '2 Pounds'
       },
       {
-        id: 2,
+        id: uuidv4(),
         name: 'Salt',
         amount: '1 Tbs'
       }
     ]
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: 'Plain Beef',
     cookTime: '0:45',
     servings: 5,
     instructions: '1- Put paprika on beef \n2- Put beef in oven \n3- Eat beef',
     ingredients: [
       {
-        id: 1,
+        id: uuidv4(),
         name: 'Beef',
         amount: '3 Pounds'
       },
       {
-        id: 2,
+        id: uuidv4(),
         name: 'Paprika',
         amount: '2 Tbs'
       }
